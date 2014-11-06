22a70d26e4c048ac168fef2f0404445e222b3c8b
NicoK78 <nicolas.kervoern@gmail.com>
2014-11-07 00:04:28 +0100

    Update game.js

--- src/js/game.js
+++ src/js/game.js
@@ -4,12 +4,25 @@ $(document).ready(function(){
 	var nbClick = 0;
 	var nbImg = 3;
 	var tabImg = [];
+	var indexTab;
+	
+	var indexImg;
+	var nameImg;
 
-	for(var i = 0; i < nbImg; i++) {
-
+	//Remplir le tableau avec le nom des images, mélangé
+	for(var i = 1; i <= nbImg; i++) {
+		indexImg = i;
+		nameImg = "memory0"+indexImg;
+		for(var j = 0; j < 2; j++) {
+			indexTab =  Math.floor(Math.random() * nbImg);
+			if(tabImg[indexTab] == null)
+				tabImg[indexTab].push(nameImg);
+			else
+				j--;
+		}
 	}
 
 	//game.write(nbClick);
 	//var monScore = document.getElementById("score");
-	$("#score").html(nbClick);
+	$("#score").html("Nombre de clics : "+nbClick);
 });
