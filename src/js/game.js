787e95ca836ab7d3ab9911975391cb8cc1c81a7a
Nicolas <nicolas.kervoern@gmail.com>
2014-11-07 11:50:54 +0100

    alea cards

--- src/js/game.js
+++ src/js/game.js
@@ -8,19 +8,33 @@ $(document).ready(function(){
 	
 	var indexImg;
 	var nameImg;
+	var linkImg;
 
 	//Remplir le tableau avec le nom des images, mélangé
 	for(var i = 1; i <= nbImg; i++) {
 		indexImg = i;
-		nameImg = "memory0"+indexImg;
+		nameImg = "img/memory0"+indexImg+".png";
+
 		for(var j = 0; j < 2; j++) {
-			indexTab =  Math.floor(Math.random() * nbImg);
+			indexTab =  Math.floor(Math.random() * (nbImg * 2));
 			if(tabImg[indexTab] == null)
-				tabImg[indexTab].push(nameImg);
+				tabImg[indexTab] = (nameImg);
 			else
 				j--;
 		}
 	}
+
+	//$(".contain").append('<img id="img" src='+tabImg[0]+'/>');
+	//$(".contain").append('<img id="img" src="img/memory01.png"/>');
+
+
+	for(var i = 0; i < tabImg.length; i++) {
+		$(".contain").append('<img id="img" src='+tabImg[i]+'/>');
+		//linkImg = 'src/img/'+tabImg[i];
+		//$(".contain").append('<img id="img" src=""+tabImg[i]+""/>');
+		//$(".contain").attr("src", linkImg);
+	}
+
 
 	//game.write(nbClick);
 	//var monScore = document.getElementById("score");
